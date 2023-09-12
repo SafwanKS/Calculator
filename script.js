b71b68dd2c22e92d97a9bf670f4048833f83a9d0
SafwanKS <144458370+SafwanKS@users.noreply.github.com>
2023-09-12 19:17:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-function bClick(val){
-    if(document.getElementById("screenText").value==0){
-        document.getElementById("screenText").value=""
-        document.getElementById("screenText").value+=val
-    }else{
-        document.getElementById("screenText").value+=val
+function bClick(val) {
+    if (document.getElementById("screen").value == 0) {
+        if (document.getElementById("screen").value >= 10) {} else {
+            document.getElementById("screen").value = ""
+            document.getElementById("screen").value += val
+        }
+
+    } else {
+        document.getElementById("screen").value += val
     }
-    
-}  
 
-
-
-
-function equalTo(){
-    var text = document.getElementById("screenText").value
-        var result = eval(text)
-        document.getElementById("screenText").value = result
 }
 
-function clearText(){
-    document.getElementById("screenText").value="0"
+
+function backClick() {
+
+    const display = document.getElementById('screen');
+    var str = display.value
+    var result = str.toString().slice(0, -1)
+    display.value = result
+
+
+
+}
+
+
+
+function equalTo() {
+
+    const display = document.getElementById('screen');
+    if (display.value == 169) {
+        display.value = 'I Love youh😚💝';
+
+        size = document.getElementById('screen');
+        size.style.fontSize = "250%";
+
+
+    } else {
+        if (display.value == 226) {
+            display.value = 'Please wait...';
+
+            size = document.getElementById('screen');
+            size.style.fontSize = "250%";
+            goPage()
+        } else {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        }
+    }
+
+}
+
+function clearText() {
+    document.getElementById("screen").value = ""
+    size = document.getElementById('screen');
+    size.style.fontSize = "350%";
+}
+
+function goPage() {
+    setTimeout(function () {
+        window.open("secondpage.html");
+    }, 3000);
 }
